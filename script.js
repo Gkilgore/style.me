a71676a88691c258dafa91a95e12d3b029d2a0c2
Gkilgore <gkilgore@frhsd.com>
2023-02-15 23:37:56 -0500

    added .js comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hide all design rules by default
 document.getElementById('designRule1').style.display = 'none'
 document.getElementById('designRule2').style.display = 'none'
 document.getElementById('designRule3').style.display = 'none'
@@ -9,9 +10,9 @@ document.getElementById('designRule8').style.display = 'none'
 document.getElementById('designRule9').style.display = 'none'
 document.getElementById('designRule10').style.display = 'none'
 
-
-
+// Function to hide all design rules
 function hideRules () {
+  // Hide each design rule by setting its display property to 'none'
   document.getElementById('designRule1').style.display = 'none'
   document.getElementById('designRule2').style.display = 'none'
   document.getElementById('designRule3').style.display = 'none'
@@ -23,14 +24,22 @@ function hideRules () {
   document.getElementById('designRule9').style.display = 'none'
   document.getElementById('designRule10').style.display = 'none'
 }
+
+// Function to hide all design rules and additional info
 function hideRulesAndInfo () {
+  // Call the hideRules function to hide all design rules
   hideRules()
+  // Hide the additional info by setting its display property to 'none'
   document.getElementById('designRule0').style.display = 'none'
 }
 
-let number = "0"
+// Declare a variable 'number' and set its value to "0"
+let number = '0'
 
-function showRule(number) {
-    hideRulesAndInfo();
-  document.getElementById('designRule'+number).style.display = 'block';
+// Function to show a specific design rule based on the number parameter passed in
+function showRule (number) {
+  // Call the hideRulesAndInfo function to hide all design rules and additional info
+  hideRulesAndInfo()
+  // Show the design rule corresponding to the number parameter passed in by setting its display property to 'block'
+  document.getElementById('designRule' + number).style.display = 'block'
 }
